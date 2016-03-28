572004e289a7d72e1a105bc1cc121df557a1ce49
Perfect-213536362 <213536362@stu.ukzn.ac.za>
2016-03-28 03:05:15 +0200

    completed

--- ConsoleApplication1/DeckOfCards.cpp
+++ ConsoleApplication1/DeckOfCards.cpp
@@ -1,31 +1,83 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
 #include "DeckOfCards.h"
+#include "Card.h"
 
-
-DeckOfCards::DeckOfCards()
+using namespace std;
+class DeckOfCards
 {
-}
+private:
+	void free();
+	void Switch(Card*, Card*);
+	int CardNum = 0;
+	int Cards;
+
+public:
+
+	DeckOfCards::DeckOfCards()
+	{
+		reset();
+	}
 
 
-DeckOfCards::~DeckOfCards()
-{
-}
-void reset()
-{
+	DeckOfCards::~DeckOfCards()
+	{
 
-}
-void shuffle()
-{
+	}
+	void reset()
+	{
+		CardNum = 20;
 
-}
-void draw()
-{
+	}
 
-}
-void peek()
-{
 
-}
-int numberOfCards()
-{
+	void shuffle()
+	{
+		if (CardNum > 0)
+		{
+			int i;
+			for (i = 0; i < 50; ++i)
+			{
+				switch (Cards[rand()% CardNum + 1], Cards[rand() % 6 + 1])
+			
+			}
 
-}
+		}
+	}
+	Card draw()
+	{
+		Card temp = peek();
+		delete Cards[CardNum - 1];
+		--CardNum;
+		return temp;
+	}
+	Card peek()
+	{
+		return *Cards[CardNum - 1];
+	}
+	int numberOfCards()
+	{
+		return CardNum;
+	}
+
+	void DeckOfCards::Switch(Card *card1, Card *card2)
+	 {
+		 Card Hold = *card1;
+		*card1 = *card2;
+		*card2 = Hold;
+	 }
+
+	void DeckOfCards::free()
+		{
+		 	int i;
+		 	for (i = 0; i < 20; ++i)
+				delete Cards[i];
+		}
+
+	DeckOfCards::~DeckOfCards()
+	     {
+			free();
+		 }
+
+};
--- ConsoleApplication1/DeckOfCards.h
+++ ConsoleApplication1/DeckOfCards.h
@@ -1,19 +1,24 @@
 #pragma once
 #ifndef DECKOFCARDS_H
 #define DECKOFCARDS_H
-
+#include<cstdlib>
 
 #include "Card.h"
 class DeckOfCards :
 	public Card
 {
+private:
+	void free();
+	void Switch(Card*, Card*);
+		int CardNum = 0;
+		int Cards;
 public:
 	DeckOfCards();
 	~DeckOfCards();
 	void reset();
 	void shuffle();
-	void draw();
-	void peek();
+	Card draw();
+	Card peek();
 	int numberOfCards();
 
 };
